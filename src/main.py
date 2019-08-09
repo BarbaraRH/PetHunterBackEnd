a1415dc39e8515ce3d5706e8d77bb2702af4db40
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-08-09 18:17:25 +0000

    c

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,7 +5,6 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db, User, Adverts, Pets
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -16,143 +12,115 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-@app.route('/user', methods=['POST', 'GET', 'DELETE'])
+@app.route('/users', methods=['POST', 'GET', 'DELETE'])
 def handle_person():
-    """
-    Create person and retrieve all persons
-    """
-
     # POST request
     if request.method == 'POST':
         body = request.get_json()
-
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
         if 'username' not in body:
             raise APIException('You need to specify the username', status_code=400)
         if 'email' not in body:
             raise APIException('You need to specify the email', status_code=400)
-
-        user1 = User(username=body['username'], email=body['email'])
+        if 'password' not in body:
+            raise APIException('You need to specify the password', status_code=400)
+        if 'firstname' not in body:
+            raise APIException('You need to specify the firstname', status_code=400)
+        if 'lastname' not in body:
+            raise APIException('You need to specify the lastname', status_code=400)
+        user1 = User(username=body['username'], email=body['email'], password=body['password'], firstname=body['firstname'], lastname=body['lastname'])
         db.session.add(user1)
         db.session.commit()
         return "ok", 200
-
     # GET request
     if request.method == 'GET':
         all_people = User.query.all()
         all_people = list(map(lambda x: x.serialize(), all_people))
         return jsonify(all_people), 200
-
+     # DELETE request
     if request.method == 'DELETE':
-        delete_all= User.query().delete()
-        db.session.delete(delete_all)
-        return jsonify(delete_all), 200
-
+        db.session.query(User).delete()
+        db.session.commit()
+        return "all Deleted", 200
     return "Invalid Method", 404
-
-@app.route('/pets', methods=['GET'])
+@app.route('/pets', methods=['GET', 'DELETE'])
 def handle_pets():
-    """
-    Create person and retrieve all persons
-    """
     # GET request
     if request.method == 'GET':
         all_pets = Pets.query.all()
         all_pets = list(map(lambda x: x.serialize(), all_pets))
-
         return jsonify(all_pets), 200
-
-    return "Invalid Method", 404
-
-
-
-
-
-@app.route('/adverts', methods=['POST', 'GET'])
-
+     # DELETE request
+    if request.method == 'DELETE':
+        db.session.query(Pets).delete()
+        db.session.commit()
+        return "all Deleted"
+@app.route('/adverts', methods=['POST', 'GET','DELETE', 'UPDATE'])
 def handle_adverts():
-    """
-    Create ad and retrieve all ads
-    """
-
     # POST request
     if request.method == 'POST':
         body = request.get_json()
-
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
-        #if 'status' not in body:
-         #   raise APIException('You need to specify the pet name', status_code=400)
+       # if 'status' not in body:
+        #    raise APIException('You need to specify the status', status_code=400)
 
         request1 = Pets(name=body['name'])
         db.session.add(request1)
         db.session.commit()
 
-
-        request2 = Adverts(status=body['status'], user_id=body['user_id'])
+        request2 = Adverts(status=body['status'])
         db.session.add(request2)
         db.session.commit()
 
+        query_the_last_pets= Pets.query.order_by(Pets.id.desc()).first()
+        Adverts.update().where(Adverts.c.pet_id==null).values(pet_id=query_the_last_pets)
 
-        return "ok", 200
-
+        return str(query_the_last_pets.id)
     # GET request
     if request.method == 'GET':
         status = request.args.get('status')
         all_adverts = Adverts.query.filter_by(status=status).order_by(Adverts.created_at.desc())
         all_adverts = list(map(lambda x: x.serialize(), all_adverts))
-
         return jsonify(all_adverts), 200
-
-    """if request.method == 'GET':
-        all_adverts = db.session.query(Adverts).join(Pets).all()
-        all_adverts = list(map(lambda x: x.serialize(), all_adverts))
-
-        return jsonify(all_adverts), 200"""
-
+        # DELETE request
+    if request.method == 'DELETE':
+        db.session.query(Adverts).delete()
+        db.session.commit()
+        return "all Deleted"
     return "Invalid Method", 404
-
 @app.route('/user/<int:user_id>', methods=['PUT', 'GET', 'DELETE'])
 def get_single_person(user_id):
     """
     Single person
     """
-
     # PUT request
     if request.method == 'PUT':
         body = request.get_json()
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
-
         user1 = User.query.get(user_id)
         if user1 is None:
             raise APIException('User not found', status_code=404)
-
         if "username" in body:
             user1.username = body["username"]
         if "email" in body:
             user1.email = body["email"]
         db.session.commit()
-
         return jsonify(user1.serialize()), 200
-
     # GET request
     if request.method == 'GET':
         user1 = User.query.get(user_id)
         if user1 is None:
             raise APIException('User not found', status_code=404)
         return jsonify(user1.serialize()), 200
-
     # DELETE request
     if request.method == 'DELETE':
         user1 = User.query.get(user_id)
@@ -161,9 +129,7 @@ def get_single_person(user_id):
         db.session.delete(user1)
         db.session.commit()
         return "ok", 200
-
     return "Invalid Method", 404
-
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT)
--- src/models.py
+++ src/models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -8,9 +7,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 from datetime import datetime
-
 db = SQLAlchemy()
-
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
@@ -21,17 +18,17 @@ class User(db.Model):
     password = db.Column(db.String(20), unique=True, nullable=True)
     photo = db.Column(db.String(300), unique=False, nullable=True)
     adverts = db.relationship('Adverts')
-
     def __repr__(self):
         return '<User %r>' % self.username
-
     def serialize(self):
         return {
+            "id": self.id,
             "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
-            "id": self.id
+            "password": self.password,
         }
-
 class Pets(db.Model):
     __tablename__ = 'pets'
     id = db.Column(db.Integer, primary_key=True)
@@ -41,16 +38,16 @@ class Pets(db.Model):
     size = db.Column(db.String(120), unique=True, nullable=True)
     photo = db.Column(db.String(300), unique=False, nullable=True)
     adverts = db.relationship('Adverts')
-
     def __repr__(self):
         return '<Pets %r>' % self.id
-
     def serialize(self):
         return {
             "name": self.name,
-            "id": self.id
-        }
-
+            "id": self.id,
+            "breed": self.breed,
+            "gender": self.id,
+            "size": self.size
+}
 class Adverts(db.Model):
     __tablename__ = 'adverts'
     id = db.Column(db.Integer, primary_key=True)
@@ -60,10 +57,8 @@ class Adverts(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     status = db.Column(db.String(80), unique=False, nullable=True)
     address = db.Column(db.String(80), unique=False, nullable=True)
-
     def __repr__(self):
-        return 'Adverts %r>' % self.id
-
+        return '<Adverts %r>' % self.id
     def serialize(self):
         return {
             "user_id": self.user_id,
@@ -71,5 +66,4 @@ class Adverts(db.Model):
             "status": self.status,
             "created_at": self.created_at
         }
-
 render_er(db.Model, 'diagram.png')
