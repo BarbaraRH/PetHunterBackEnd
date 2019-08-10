7803e9d208ae0a98ba1850b73d82b22ed4696006
GuillermoPiedrabuena <gnpiedrabuena@miuandes.cl>
2019-08-10 16:10:48 +0000

    se creo carpeta upload

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, render_template
+from werkzeug import secure_filename
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
@@ -7,6 +8,8 @@ from utils import APIException, generate_sitemap
 from models import db, User, Adverts, Pets
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config['UPLOAD_FOLDER'] = "./upload"
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
@@ -18,6 +21,19 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route('/upload', methods=['GET', 'POST'])#ruta guardar fotos
+def upload():
+    # GET request
+    if request.method == 'POST':
+        f= request.file["file"]
+        filename=secure_filename(f.filename)
+        f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return "subido con exito"
+
+
+
+
 @app.route('/users', methods=['POST', 'GET', 'DELETE'])
 def handle_person():
     # POST request
